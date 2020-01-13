e3f66c78e099ef4055fe996a3f8ed00eea1704ab
John Reiner <johnreiner1990@gmail.com>
2020-01-13 17:21:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,3 +18,8 @@ end
 def reduce_to_total(source_array, starting_point = 0)
     source_array.reduce(starting_point) { |sum, num| sum + num}
 end
+
+def reduce_to_all_true(array)
+  if array
+    array.reduce(0) { |sum, num| sum + num}
+end
