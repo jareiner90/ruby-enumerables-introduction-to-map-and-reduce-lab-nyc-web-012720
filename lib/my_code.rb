649ac62a28e7a82b1ec5a191ce48ea572d32266c
John Reiner <johnreiner1990@gmail.com>
2020-01-13 17:45:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,5 +20,5 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-   return source_array.reduce{ |x, point| !!x && !!point }
+   return source_array.reduce{ |i, x| !!i && !!x }
 end
